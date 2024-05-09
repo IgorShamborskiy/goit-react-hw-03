f4aecd42478f80032fede0468a37c8c89dc556b8
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2024-05-09 23:51:38 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,25 +1,31 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 import { nanoid } from "nanoid";
 import { useId } from "react";
-
-
+const FeedbackSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too short!').max(50, 'Too long!').required('Required'),
+  number: Yup.string().min(3, 'Too short!').max(50, 'Too long!').required('Required'),
+});
 
 export default function ContactForm ({onAdd}) {
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        onAdd({
-            id:Date.now(),
-            text:evt.target.elements.text.value,
-            number:evt.target.elements.tel.value,
-        });
-        evt.target.reset();
-      };
+  const contactId = useId();
+  const initialValues = {
+      name: '',
+      number: '',
+  };
+  const handleSubmit = (values, actions) => {
+    onAdd({ ...values, id: contactId });
+      actions.resetForm();
+  }
     return (
-      <form onSubmit={handleSubmit}>
+<Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
+<form>
         <label>Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="text" />
         <label>Number</label>
-        <input type="tel" name="number" />
+        <input type="text" name="number" />
         <button type="submit">Add contact</button>
       </form>
+</Formik>
     );
   };
